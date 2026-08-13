17d81af0000355b368bf02af71c6af34fc9ff442
gustavosilvaferreira14-cloud <gustavo.silva.ferreira14@escola.pr.gov.br>
2026-08-13 08:47:44 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+//Botão e curtidas
+document.addEventListener("DOMContentLoader", () =>{
+    const likeBtn = document.querySelector("left-actions . actions-btn:fist-child");
+    if (!likeBtn) return;
+const likeSvg = likeBtn.querySelector ("svg");
+ 
+//locaiza o contador
+let textNode = Array
+
+
+
+
+})
+
+
