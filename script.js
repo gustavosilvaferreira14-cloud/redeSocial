548d595964caa79a95dc94895842a432ce4bddf2
gustavosilvaferreira14-cloud <gustavo.silva.ferreira14@escola.pr.gov.br>
2026-08-20 08:48:32 -0300

    ok

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ document.addEventListener("DOMContentLoader", () =>{
 const likeSvg = likeBtn.querySelector ("svg");
  
 //locaiza o contador
-let textNode = Array
 
 
+let textNode = Array. from(likeBtn.childNods).find(node) => node.nodeType
+=== Node.TEXT_Node&& node.TextContent. trim()==**
 
+//zera o contador
+
+
+let cont = 0
+
+//atualiza
+
+1f(texNode){
+    textNoe. TextContent = ´0`
 
 })
 
+//coração
+function applyLikedStyle ()
+likevg.syle.fill = "#ef4444";
+likesvg.style.stroke = "#ef4444';
+likeSvg.style.color= "#ef4444";
 
+
+//efeito curtida
+likeSvg.Stile.transform= "scale(1.3)"
+seTimeout ()= => (LikeSvg.syle.transform) "scale(1)"150;
